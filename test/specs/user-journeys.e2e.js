6eba8278c2d326c480cf862714e5fca6d91f7830
Pavel Saman <samanpavel@gmail.com>
2021-11-26 02:45:33 +0100

    update journey

--- test/specs/user-journeys.e2e.js
+++ test/specs/user-journeys.e2e.js
@@ -40,14 +40,9 @@ const addToCart = async () => {
     .click();
 };
 
-describe('User journeys', () => {
+describe('User journey', () => {
 
   beforeEach(async () => {
-    await browser
-      .sendCommand('Network.clearBrowserCookies', {});
-    await browser
-      .sendCommand('Network.clearBrowserCache', {});
-
     await browser
       .setWindowSize(1920, 1080);
   });
@@ -56,22 +51,76 @@ describe('User journeys', () => {
 
     await browser
       .url(browser.config.baseUrl);
+
+    await expect($('.c-category-tiles__item'))
+      .toBeDisplayedInViewport();
   });
 
-  it('Performance audit', async () => {
+  /*it('Performance audit', async () => {
     await browser.performAudit(await browser.getUrl());
+  });*/
+
+  it('Go to category', async () => {
+
+    const menuItem = await $('a[href="/nove-skladem"]');
+    await menuItem
+      .click();
+
+    await expect(browser)
+      .toHaveUrlContaining('/nove-skladem');
   });
 
-  it('Search product and put one item to cart', async () => {
+  it('Open login popup', async () => {
 
-    await browser
-      .url(browser.config.baseUrl);
+    const myProfileBtn = await $('[href="/muj-profil"]');
+    await myProfileBtn
+      .click();
+
+    const email = await $('#Email');
+    await expect(email)
+      .toBeDisplayedInViewport();
+  });
+
+  it('Fill in credentials', async () => {
+
+    const email = await $('#Email');
+    await email
+      .setValue('samanpavel+01@gmail.com');
+
+    const pwd = await $('#Password')
+    await pwd
+      .setValue('12345a');
+  });
+
+  it('Log in', async () => {
+
+    const loginBtn = await $('.o-button');
+    await loginBtn
+      .click();
+      
+    await expect($('.c-flash-message--info'))
+      .toBeDisplayedInViewport();
+
+    await expect($('#c-product-list__item-container'))
+      .toBeDisplayedInViewport();
+  });
+
+  it('Search product', async () => {
 
     await lookForProduct('ponožky');
-      
+  });
+
+  it('Choose product', async () => {
+
     await chooseProduct(1);
+  });
+
+  it('Open product popup', async () => {
 
     await addToCart();
+  });
+
+  it('Go to cart', async () => {
 
     await expect(browser)
       .toHaveUrlContaining('/kosik');
@@ -80,125 +129,99 @@ describe('User journeys', () => {
       .toBeDisplayedInViewport();
   });
 
-  it('Log in and log out', async () => {
-
-    await browser
-      .url(browser.config.baseUrl);
-
-    const myProfileBtn = await $('[href="/muj-profil"]')
-    await myProfileBtn
-      .click();
-
-    const email = await $('#Email')
-    await email
-      .setValue(browser.config.username);
-
-    const pwd = await $('#Password')
-    await pwd
-      .setValue(browser.config.password);
-
-    const loginBtn = await $('.o-button')
-    await loginBtn
-      .click();
-      
-    await expect($('.c-flash-message--info'))
-      .toBeDisplayedInViewport();
-
-    const myProfileLoggedBtn = await $('.c-header__my-account')
-    await myProfileLoggedBtn
-      .click();
-
-    const logoutBtn = await $('.c-user-menu__logout')
-    await logoutBtn
-      .click();
-
-    await expect($('.c-flash-message--info'))
-      .toBeDisplayedInViewport();
-
-    await expect($('#hp-banner'))
-      .toBeDisplayedInViewport();
-  });
-
-  it('Go to new items category', async () => {
-
-    await browser
-      .url(browser.config.baseUrl);
-
-    const menuItem = await $('a[href="/nove-skladem"]')
-    await menuItem
-      .click();
-
-    await expect(browser)
-      .toHaveUrlContaining('/nove-skladem');
-  });
-
-  it('Create order', async () => {
-    await browser
-      .url(browser.config.baseUrl);
-
-    await lookForProduct('ponožky');
-
-    await chooseProduct(1);
-
-    await addToCart();
+  it('Go to 2nd step in cart', async () => {
 
     const continueTo2ndStepBtn = await $('[href="/doprava-a-platba"]')
     await continueTo2ndStepBtn
       .click();
 
+    await expect(browser)
+      .toHaveUrlContaining('/doprava-a-platba');
+  });
+
+  it('Choose shipping', async () => {
+
     const shippingMethod = await $(`[for="${order.shippingMethod}"]`)
     await shippingMethod
       .click();
 
-    let summaries = await $$('.c-summary-box__footer-item-label');
-    await expect(summaries[summaries.length - 1])
-      .toHaveText('DPD');
+      await browser.waitUntil(
+        async () => {
+          const summaries = await $$('.c-summary-box__footer-item-label');
+          const summariesTexts = await Promise.all(summaries.map(async el => {
+            const text = await el.getText();
+            return text;
+          }));
+          return summariesTexts.includes('DPD') === true;
+        }
+      );
+  });
 
-    const paymentMethod = await $(`[for="${order.paymentMethod}"]`)
+  it('Choose payment', async () => {
+
+    const paymentMethod = await $(`[for="${order.paymentMethod}"]`);
     await paymentMethod
       .click();
 
-    summaries = await $$('.c-summary-box__footer-item-label');
-    await expect(summaries[summaries.length - 1])
-      .toHaveText('Bankovní převod');
+    await browser.waitUntil(
+      async () => {
+        const summaries = await $$('.c-summary-box__footer-item-label');
+        const summariesTexts = await Promise.all(summaries.map(async el => {
+          const text = await el.getText();
+          return text;
+        }));
+        return summariesTexts.includes('Bankovní převod') === true;
+      }
+    );
+  });
 
-    const goToPersonalInfoBtn = await $('[href="/osobni-udaje"]')
+  it('Go to 3rd step in cart', async () => {
+
+    const goToPersonalInfoBtn = await $('[href="/osobni-udaje"]');
     await goToPersonalInfoBtn
       .click();
 
-    const firstName = await $('#FirstName')
+    await expect(browser)
+      .toHaveUrlContaining('/osobni-udaje');
+  });
+
+  it('Fill in personal info', async () => {
+    const firstName = await $('#FirstName');
     await firstName
       .setValue(order.name);
 
-    const lastName = await $('#LastName')
+    const lastName = await $('#LastName');
     await lastName
       .setValue(order.surname);
 
-    const street = await $('#Street')
+    const street = await $('#Street');
     await street
       .setValue(order.street);
 
-    const city = await $('#City')
+    const city = await $('#City');
     await city
       .setValue(order.city);
 
-    const zip = await $('#ZipCode')
+    const zip = await $('#ZipCode');
     await zip
       .setValue(order.zip);
 
-    const email = await $('#Email')
+    const email = await $('#Email');
     await email
       .setValue(order.email);
 
-    const phone = await $('#Phone')
+    const phone = await $('#Phone');
     await phone
       .setValue(order.phone);
 
-    const consent = await $('[for="OrderConsents_0__IsChecked"]')
+    const consent = await $('[for="OrderConsents_0__IsChecked"]');
     await consent
       .click();
+  });
 
-    const submitBtn = await $('#submit-button')
+  it('Confirm order', async () => {
+
+    const submitBtn = await $('#submit-button');
     await submitBtn
       .click();
 
